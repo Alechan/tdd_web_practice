3d82630fd8fe298a4c42025c005b6955dd82e8e1
Alechan <mail@domain.com>
2020-08-28 19:42:41 -0300

    -

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.contrib import auth
 from django.db import models
+from django.urls import reverse
 
 auth.signals.user_logged_in.disconnect(auth.models.update_last_login)
 
@@ -18,6 +19,17 @@ class User(models.Model):
 
 
 class Token(models.Model):
+
     email = models.EmailField()
     uid   = models.CharField(default=uuid.uuid4, max_length=40)
 
+    @classmethod
+    def from_email(cls, email, request):
+        token = Token.objects.create(email=email)
+        url = request.build_absolute_uri(
+            reverse("login") + '?token=' + str(token.uid)
+        )
+        return url
+
+    class Meta:
+        unique_together = ('email', 'uid',)
--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,29 +1,48 @@
 from django.contrib import auth
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 from accounts.models import Token
+
+EMAIL = 'a@b.com'
 
 User = get_user_model()
 
 
 class UserModelTest(TestCase):
     def test_user_is_valid_with_email_only(self):
-        user = User(email='a@b.com')
-        user.full_clean()  # should not raise
+        user = User(email=EMAIL)
+        try:
+            user.full_clean()  # should not raise
+        except ValidationError:
+            self.fail("An email should be enough to create a User.")
 
     def test_email_is_primary_key(self):
-        user = User(email='a@b.com')
-        self.assertEqual(user.pk, 'a@b.com')
+        user = User(email=EMAIL)
+        self.assertEqual(user.pk, EMAIL)
 
     def test_no_problem_with_auth_login(self):
-        user = User.objects.create(email='edith@example.com')
+        user = User.objects.create(email=EMAIL)
         user.backend = ''
         request = self.client.request().wsgi_request
         auth.login(request, user)  # should not raise
 
+
 class TokenModelTest(TestCase):
     def test_links_user_with_auto_generated_uid(self):
-        token1 = Token.objects.create(email='a@b.com')
-        token2 = Token.objects.create(email='a@b.com')
+        token1 = Token.objects.create(email=EMAIL)
+        token2 = Token.objects.create(email=EMAIL)
         self.assertNotEqual(token1.uid, token2.uid)
+
+    def test_token_creation_from_email_returns_valid_url(self):
+        request = self.client.request().wsgi_request
+        url = Token.from_email(EMAIL, request)
+        self.validate_test_url(url)
+
+    def validate_test_url(self, test_url):
+        validator = URLValidator()
+        self.assertIn("testserver", test_url)
+        url = test_url.replace("testserver", "testserver.com")
+        validator(url)
