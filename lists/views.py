52948f82600fba393e747ab9709ba8362a251fdd
Alechan <mail@domain.com>
2020-08-29 03:43:26 -0300

    lists can have owners, which are saved on creation

--- lists/views.py
+++ lists/views.py
@@ -26,11 +26,14 @@ def view_list(request, list_id):
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
+        list_ = List()
+        if request.user.is_authenticated:
+            list_.owner = request.user
+        list_.save()
         form.save(for_list=list_)
         return redirect(list_)
     else:
-        return render(request, "home.html", {"form": form})
+        return render(request, 'home.html', {"form": form})
 
 
 def my_lists(request, email):
